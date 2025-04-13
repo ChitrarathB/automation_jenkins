05d86e378ce6a2de7835b5040dadc8ed322868b7
Chitrarath Bhattacharjee <chitrarathb@gmail.com>
2025-04-13 19:11:46 +0800

    Update Project

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,12 +7,14 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions,hooks")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/cucumber-html-report.html, json:target/cucumber-reports/cucumber-json-report.json, com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@test")
 public class TestRunner {
     // This class is empty, all configuration is done through annotations
     // For report generation, use the ScreenshotReportGenerator utility class
--- src/test/resources/features/WebsiteNavigation.feature
+++ src/test/resources/features/WebsiteNavigation.feature
@@ -1,8 +1,18 @@
 Feature: Navigate and Verify Example Website
 
+  @test
   Scenario: Navigate to The Internet demo site and verify elements
     Given I navigate to The Internet demo site
     When I click on the Form Authentication link
     Then I should be on the login page
     When I enter username "tomsmith" and password "SuperSecretPassword!"
     Then I should see a success message
+
+  @test2
+  Scenario: Navigate to The Internet demo site and verify elements
+    Given I navigate to The Internet demo site
+    When I click on the Form Authentication link
+    Then I should be on the login page
+    When I enter username "test2" and password "pass2!"
+    Then I should see a success message
+
